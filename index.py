414c38fda246483dcfcc8a10dad8dbd37158a3dc
Guyo-deepsea <samuelguyo21@gmail.com>
2024-05-19 16:01:25 +0300

    new changes

--- index.py
+++ index.py
@@ -4,17 +4,28 @@ import streamlit as st
 import seaborn as sns
 from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
+import time
+
+st.set_page_config(page_title="Data Profiling")
 
 # title
-st.markdown('''# **Exploratory Data Analysis**''')
+st.title('Exploratory Data Analysis')
+
+st.markdown('''---''')
 
 # uploadinf file section
 with st.sidebar.header("Upload your dataset(.csv)"):
     uploaded_file = st.sidebar.file_uploader("Upload your file", type=['csv'])
     df = sns.load_dataset('titanic')
-    st.sidebar.markdown("[Example csv file](df)")
+ # st.sidebar.markdown("[Example csv file](df)")
 # profiling report
+
+
 if uploaded_file is not None:
+
+    with st.spinner(text='In progress'):
+        time.sleep(3)
+
     def load_csv():
         csv = pd.read_csv(uploaded_file)
         return csv
@@ -27,4 +38,10 @@ if uploaded_file is not None:
     st.header('**PROFILE**')
     st_profile_report(pr)
 else:
-    st.warning("please upload your file")
+    st.warning(" ## ***please upload your csv file for Data Analysis***")
+
+
+footer_html = """<div style='text-align: center;'>
+  <p>Developed ❤️ by Guyo </p>
+</div>"""
+st.markdown(footer_html, unsafe_allow_html=True)
